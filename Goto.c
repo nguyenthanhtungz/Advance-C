dbc4786a2cbc9a1f4cbd4928023c70742c072c6c
nguyenthanhtungz <113908920+nguyenthanhtungz@users.noreply.github.com>
2022-10-04 05:48:01 +0700

    Update Goto.c

--- Goto.c
+++ Goto.c
@@ -4,32 +4,4 @@
 
 
 
-#include <Arduino.h>
-#include <stdlib.h>
-uint8_t ledPin[] = {6,7,8,9}; // Mảng lưu vị trí các chân Digital mà các đèn LED sử dụng theo thứ tự từ 1->8. 
-uint8_t pinCount; 
-#define ButtonBlinkLed 5
-#define ButtonSangDuoi 4
 
-void TurnOnAllLed (){
-  for (int i = 0; i < pinCount; i++)
-  {
-  
-    digitalWrite(ledPin[i], HIGH);
-  
-  }
-  
-}
-
-void TurnOnAllLed (){
-  for (int i = 0; i < pinCount; i++)
-  {
-  
-    digitalWrite(ledPin[i], LOW);
-  
-  }
-  
-}
-
-
-void blinkLed ();
